8cd1a071c880d2517132dfa02a744643de386d29
Marina <ejiwe83@ya.ru>
2023-08-19 16:05:13 +0200

    lessoon_2_commit1

--- lesson 2/lesson_2_task_10.py
+++ lesson 2/lesson_2_task_10.py
@@ -1,10 +1,15 @@
-def bank(x=int(input()),y=int(input())):
+def bank(x=int(input("введите сумму: ")),y=int(input("введите количество лет: "))):
     x = [x]
     percent = 10
     for i in range(y):
         for i in x:
             i = int(i * (1 + percent / 100))
             x = [i]
-    print(x[0])
+    if 1 < y < 5 :
+        print("на вашем счету будет ", x[0], " через ", y, " года")
+    elif y == 1: 
+        print("на вашем счету будет ", x[0], " через ", y, " год")
+    else: 
+        print("на вашем счету будет ", x[0], " через ", y, " лет")
     
 bank() 
